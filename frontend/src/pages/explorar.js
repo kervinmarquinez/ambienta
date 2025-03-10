ff4305445df2f3b74cd172fc85ee187af74b39b6
Adrián Kervin Marquinez <kervinmarquinez@gmail.com>
2025-03-10 19:06:44 +0100

    Creado página de Explorar

--- frontend/src/pages/explorar.js
+++ frontend/src/pages/explorar.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import ProtectedRoute from "../components/ProtectedRoute";
+import Layout from "@/layout/Layout";
+import ExplorePostsList from "@/components/ExplorePostsList";
 
 export default function Explorar() {
   return (
-    <ProtectedRoute>
-      <div>
-        <h1>Explorar</h1>
- 
+    <Layout welcomeMessage="Explora todas las publicaciones">
+      <div className="mb-6">
+        <h1 className="text-2xl font-bold mb-4">Explora</h1>
+        <p className="text-gray-600 font-light">
+          Descubre publicaciones de todos los usuarios y encuentra nuevas ideas para tu hogar.
+        </p>
       </div>
-    </ProtectedRoute>
-  )
+      
+      <ExplorePostsList />
+    </Layout>
+  );
 }
